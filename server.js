8eb461d375611dce90c2caa4ec610f2dde58fcd0
ptozax <38245998+ptozax@users.noreply.github.com>
2024-12-30 17:41:19 +0700

    init

---
+++ server.js
@@ -0,0 +1,63 @@
+// Install the necessary package first by running:
+// npm install ws
+
+const WebSocket = require('ws');
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+});
+
+const wss = new WebSocket.Server({ server });
+
+let connections = {};
+
+function onConnect(socket){
+  const connection={
+    UID:`${Date.now()}_${Math.random().toString(36).substring(2, 8)}`,
+    socket:socket
+  }
+  connections[connection.UID]=connection;
+  socket.send(JSON.stringify({type:'UID',UID:connection.UID}));
+}
+
+
+wss.on('connection', (socket) => {
+  console.log('A player connected.');
+onConnect(socket);
+
+  socket.on('message', (message) => {
+    const data = JSON.parse(message);
+
+
+    if (data.type === 'chat') {
+
+      console.log('Received message:', data.message ,"---",data.UID);
+    }
+
+
+  });
+
+  socket.on('close', () => {
+    console.log('A player disconnected.');
+
+
+  });
+});
+
+server.listen(8080);
+
